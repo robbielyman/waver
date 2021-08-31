53575c93ea9b6c4f874c75ed19b5e7f2d8b3f4d1
Rylee Lyman <rylee.lyman@tufts.edu>
2021-08-31 15:17:23 -0400

    minimize prints

--- lib/tracks.lua
+++ lib/tracks.lua
@@ -15,10 +15,12 @@ function Track:buffer_render()
     softcut.buffer_clear()
     softcut.buffer_read_mono(self.file,0,0,-1,1,1,0,self.level)
     softcut.event_render(function(_,_,_,samples) 
-        print("track " .. self.id .. " got a callback")
-        self.samples = samples 
-        self.waiting_for_samples = false
-        callback_inactive = true
+        if not callback_inactive then
+            print("track " .. self.id .. " got a callback")
+            self.samples = samples 
+            self.waiting_for_samples = false
+            callback_inactive = true
+        end
     end)
     softcut.render_buffer(1,0,30,128)
     fn.dirty_scene(true)
