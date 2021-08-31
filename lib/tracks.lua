965bb2d81b843ccec9643ea120ab91682c1be14f
Rylee Lyman <rylee.lyman@tufts.edu>
2021-08-30 23:41:00 -0400

    try debugging

--- lib/tracks.lua
+++ lib/tracks.lua
@@ -11,8 +11,9 @@ Track = {
 function Track:buffer_render()
     softcut.buffer_clear()
     softcut.buffer_read_mono(self.file,0,0,-1,1,1,0,self.level)
-    softcut.event_render(function(_,_,_,samples) self.samples = samples end)
-    softcut.render_buffer(1,0,-1,128)
+    -- softcut.event_render(function(_,_,_,samples) self.samples = samples end)
+    -- softcut.render_buffer(1,0,-1,128)
+    print("haha! you found the bug!")
 end
 
 function Track:new(file, level, pan, id)
