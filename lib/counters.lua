fa3adf423f27fb1a443fd2e777c0e276c3cc73c4
Rylee Lyman <rylee.lyman@tufts.edu>
2021-08-31 14:24:44 -0400

    add scene counter

--- lib/counters.lua
+++ lib/counters.lua
@@ -4,6 +4,10 @@ function counters.init()
     counters.ui = metro.init(counters.screenminder,1/15)
     counters.ui.frame = 1
     counters.ui:start()
+
+    counters.transport = metro.init(counters.sceneminder,1/15)
+    counters.transport.frame = 1    
+    counters.transport:start()
 end
 
 function counters.screenminder()
@@ -11,6 +15,13 @@ function counters.screenminder()
         counters.ui.frame = counters.ui.frame + 1
     end
     redraw()
+end
+
+function counters.sceneminder()
+    if counters.transport ~= nil then   
+        counters.transport.frame = counters.transport.frame + 1 
+    end 
+    redraw_scene()
 end
 
 return counters
