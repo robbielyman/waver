e54a7babced879c1a2f7f8199feb77f40079802c
Rylee Lyman <rylee.lyman@tufts.edu>
2021-09-02 22:22:26 -0400

    smaller

--- lib/page.lua
+++ lib/page.lua
@@ -6,11 +6,12 @@ function page:song_view()
     for i, track in ipairs(tracks) do
         local x_pos = 0
         for _, s in ipairs(track.samples) do
-            local height = util.round(math.abs(s) * waveform_height)
+            local height = util.round(math.abs(s) * 
+                (waveform_height + 5 and i == fn.active_track() or 0))
             graphics:mlrs(x_pos, y_pos - height, 0, 2*height, i == fn.active_track() and 8 or 4)
             x_pos = x_pos + 1
         end
-        y_pos = y_pos + waveform_height
+        y_pos = y_pos + waveform_height + 6 and i == fn.active_track() or 1
     end
     graphics:teardown()
 end
@@ -21,8 +22,8 @@ function page:render()
 end
 
 function page.init()
-  waveform_height = 30
-  waveform_pos = 20
+  waveform_height = 10
+  waveform_pos = 10
   print("page_init finished")
 end
 
