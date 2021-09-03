c71cd7bc4a224261d3dc23a128c7edcf3f5ebe53
Rylee Lyman <rylee.lyman@tufts.edu>
2021-09-02 22:06:11 -0400

    was this the problem?

--- lib/page.lua
+++ lib/page.lua
@@ -3,12 +3,12 @@ page = {}
 function page:song_view()
     graphics:setup()
     local y_pos = waveform_pos
-    for i = 1, num_tracks do
-        local track = tracks[i]
+    for i, track in ipairs(tracks) do
         local x_pos = 0
         for _, s in ipairs(track.samples) do
             local height = util.round(math.abs(s) * waveform_height)
             graphics:mlrs(x_pos, y_pos - height, 0, 2*height, i == fn.active_track() and 8 or 4)
+            x_pos = x_pos + 1
         end
         y_pos = y_pos + waveform_height
     end
