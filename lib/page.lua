4e47cc81bf7f33b5bebcb720e534b60a38105568
Rylee Lyman <rylee.lyman@tufts.edu>
2021-09-10 00:00:11 -0400

    add loop markers

--- lib/page.lua
+++ lib/page.lua
@@ -2,14 +2,22 @@ page = {}
 
 function page:song_view()
     graphics:setup()
+    -- displays the current window as proportion of track length
     local miniwindow_start = util.round(window_start / track_length * 128)
     local miniwindow_end = util.round(window_end / track_length * 128)
+    graphics:mlrs(miniwindow_start, 0, miniwindow_end, 1, 2)
+    -- add minimap indicator of loop start and end
+    local miniloop_start = util.round(loop_start / track_length * 128)
+    local miniloop_end = util.round(loop_end / track_length * 128)
+    graphics:mlrs(miniloop_start, 0, 0, 2, 5)
+    graphics:mlrs(miniloop_start, 2, 0, 1, 5)
+    graphics:mlrs(miniloop_end, 0, 0, 2, 5)
+    graphics:mlrs(miniloop_end, 2, 0, -1, 5)
     local playhead = 0
     if fn.looping() then
         playhead = util.round((counters.ui.frame / counters.ui.fps) * 128/(loop_end - loop_start)% 128)
     end
     graphics:mlrs(playhead, 0, 0, (num_tracks + 1) * waveform_height)
-    graphics:mlrs(miniwindow_start, 0, miniwindow_end, 1, 4)
     local y_pos = 0
     for i, track in ipairs(tracks) do
         local x_pos = 0
