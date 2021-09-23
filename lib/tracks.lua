cf6565ffc05fb1305810455ead9e11db5bc1e494
Rylee Lyman <rylee.lyman@tufts.edu>
2021-09-22 23:58:08 -0400

    try putting scratch_track:render_buffer() after init

--- lib/tracks.lua
+++ lib/tracks.lua
@@ -32,25 +32,6 @@ function Track:buffer_render()
     fn.dirty_scene(true)
 end
 
-function scratch_track:buffer_render()
-    callback_inactive = false
-    softcut.event_render(function(_,_,_,samples)
-        if not callback_inactive then
-            print("scratch track got a callback for render call " .. self.waiting_for_samples)
-            if self.waiting_for_samples == 1 then
-                self.samples = samples
-            else
-                for i = 1, 60*128 do
-                    self.samples[#self.samples + 1] = samples[i]
-                end
-            end
-            self.waiting_for_samples = self.waiting_for_samples == 5 and -1 or self.waiting_for_samples + 1
-            callback_inactive = true
-            fn.dirty_screen(true)
-        end
-    end)
-    softcut.render_buffer(2,(self.waiting_for_samples - 1)*60,(self.waiting_for_samples)*60,60*128)
-end
 
 function Track:new(file, level, pan, id)
     local t = setmetatable({}, { __index = Track })
@@ -74,6 +55,25 @@ function tracks.init()
     scratch_track.waiting_for_samples = 0
 end
 
+function scratch_track:buffer_render()
+    callback_inactive = false
+    softcut.event_render(function(_,_,_,samples)
+        if not callback_inactive then
+            print("scratch track got a callback for render call " .. self.waiting_for_samples)
+            if self.waiting_for_samples == 1 then
+                self.samples = samples
+            else
+                for i = 1, 60*128 do
+                    self.samples[#self.samples + 1] = samples[i]
+                end
+            end
+            self.waiting_for_samples = self.waiting_for_samples == 5 and -1 or self.waiting_for_samples + 1
+            callback_inactive = true
+            fn.dirty_screen(true)
+        end
+    end)
+    softcut.render_buffer(2,(self.waiting_for_samples - 1)*60,(self.waiting_for_samples)*60,60*128)
+end
 function scratch_track:load(file)
     if file == "cancel" then
         return
