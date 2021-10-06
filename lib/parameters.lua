ed90e158374609fc43b1205374f62f76e54b502e
Rylee Lyman <rylee.lyman@tufts.edu>
2021-10-05 20:16:43 -0400

    fix: add beats

--- lib/parameters.lua
+++ lib/parameters.lua
@@ -1,7 +1,6 @@
 parameters = {}
 
 function parameters.init()
-    params:add_separator("")
     params:add_separator(" ~ w a v e r ~ ")
 
     params:add_trigger("save", "save track")
@@ -16,7 +15,17 @@ function parameters.init()
         min  = 20,
         max  = 400,
         default = 120,
-        action = function(_) fn.dirty_scene(true) end,
+        action = function(_) fn.dirty_screen(true) end,
+    }
+
+    params:add {
+        type = "number",
+        id   = "beats",
+        name = "beats per bar",
+        min  = 1,
+        max  = 16,
+        default = 4,
+        action = function(_) fn.dirty_screen(true) end,
     }
 
     params:add_separator(" ! w a r n i n g ! ")
